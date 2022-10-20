cb2eb0f84556a14f67b21c35912556d9f8323218
LucasRosa53 <lucasrosa223@hotmail.com>
2022-10-20 18:09:07 -0300

    resolvido requisito 1

--- src/average.js
+++ src/average.js
@@ -11,8 +11,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (numeros) => {
+  let soma = 0;
+  let media = 0;
 
+if (numeros.length === 0) {
+  return undefined;
+} 
+  for (let index = 0; index < numeros.length; index += 1) {
+    soma += numeros[index];
+    
+    if (typeof numeros[index] !== 'number') {
+      return undefined;
+    }
+    media = Math.round(soma / numeros.length);
+  }
+  return media;
+};
 module.exports = average;
-
-// compare pull request
